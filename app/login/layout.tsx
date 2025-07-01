0793bfe45144dd778ec68b8b4b0611eb63e59325
riishabhraj <rishabhraj5421@gmail.com>
2025-07-01 16:57:27 +0530

    updated login layout

---
+++ app/login/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function LoginLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="login-container">
+            {children}
+        </div>
+    );
+}
